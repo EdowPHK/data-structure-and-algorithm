54371fc77d3805b96ef765a51157dab679c11fbc
EdowPHK <edowphk@gmail.com>
2026-06-11 13:39:56 +0800

    Add Hot100 T1-C++

---
+++ Hot100/1.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashmap;
+
+        for (int i = 0; i < nums.size(); i++){
+            int need = target - nums[i];
+
+            if (hashmap.count(need)){
+                return {hashmap[need], i};
+            }
+            hashmap[nums[i]] = i;
+        }
+        return {};
+    }
+};
